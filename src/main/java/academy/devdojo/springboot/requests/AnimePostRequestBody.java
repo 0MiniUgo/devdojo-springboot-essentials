789dc87adf638ef4a8b4f994418c2e965d86fd0c
0MiniUgo <hugorodrigues969@gmail.com>
2022-08-08 16:14:39 -0300

    Trasactional e validação de campos

--- src/main/java/academy/devdojo/springboot/requests/AnimePostRequestBody.java
+++ src/main/java/academy/devdojo/springboot/requests/AnimePostRequestBody.java
@@ -2,7 +2,10 @@ package academy.devdojo.springboot.requests;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 public class AnimePostRequestBody {
+    @NotBlank(message = "The anime name cannot be null or empty")
     private String name;
 }
--- src/main/java/academy/devdojo/springboot/requests/AnimePutRequestBody.java
+++ src/main/java/academy/devdojo/springboot/requests/AnimePutRequestBody.java
@@ -2,8 +2,12 @@ package academy.devdojo.springboot.requests;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 public class AnimePutRequestBody {
+    @NotBlank(message = "The anime id cannot be null or empty")
     private Long id;
+    @NotBlank(message = "The anime name cannot be null or empty")
     private String name;
 }
