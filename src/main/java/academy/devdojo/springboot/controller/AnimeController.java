524b34b0e6b5bde177ba608c396eabfc7df7b302
0MiniUgo <hugorodrigues969@gmail.com>
2022-08-08 10:16:32 -0300

    Create project

---
+++ src/main/java/academy/devdojo/springboot/controller/AnimeController.java
@@ -0,0 +1,29 @@
+package academy.devdojo.springboot.controller;
+
+import academy.devdojo.springboot.domain.Anime;
+import academy.devdojo.springboot.util.DateUtil;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@RequestMapping("/animes")
+@Log4j2
+public class AnimeController {
+
+    private final DateUtil dateUtil;
+
+    public AnimeController(DateUtil dateUtil) {
+        this.dateUtil = dateUtil;
+    }
+
+    @GetMapping
+    public List<Anime> list() {
+        log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
+        return List.of(new Anime("DBZ"), new Anime("One Piece"));
+    }
+}
