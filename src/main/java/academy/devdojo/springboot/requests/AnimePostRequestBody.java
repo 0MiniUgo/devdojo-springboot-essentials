9c2d6e1ff5d9f85c07196950a5b250331aef9bf3
0MiniUgo <hugorodrigues969@gmail.com>
2022-08-09 14:52:10 -0300

    Unit Tests pt 02

--- src/main/java/academy/devdojo/springboot/requests/AnimePostRequestBody.java
+++ src/main/java/academy/devdojo/springboot/requests/AnimePostRequestBody.java
@@ -1,10 +1,12 @@
 package academy.devdojo.springboot.requests;
 
+import lombok.Builder;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
 
 @Data
+@Builder
 public class AnimePostRequestBody {
     @NotBlank(message = "The anime name cannot be null or empty")
     private String name;
--- src/main/java/academy/devdojo/springboot/requests/AnimePutRequestBody.java
+++ src/main/java/academy/devdojo/springboot/requests/AnimePutRequestBody.java
@@ -1,10 +1,12 @@
 package academy.devdojo.springboot.requests;
 
+import lombok.Builder;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
 
 @Data
+@Builder
 public class AnimePutRequestBody {
     @NotBlank(message = "The anime id cannot be null or empty")
     private Long id;
