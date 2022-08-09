9c538555e991bd9763725a402d0dc02f64f3db87
0MiniUgo <hugorodrigues969@gmail.com>
2022-08-09 17:18:43 -0300

    Testes de Integração pt 02

--- src/main/java/academy/devdojo/springboot/requests/AnimePostRequestBody.java
+++ src/main/java/academy/devdojo/springboot/requests/AnimePostRequestBody.java
@@ -1,12 +1,16 @@
 package academy.devdojo.springboot.requests;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class AnimePostRequestBody {
     @NotBlank(message = "The anime name cannot be null or empty")
     private String name;
